41df5e3c2240f2d42e72d47205138d8db36f8611
@webdeveloper130390 <abdullokhpakhlavonov@gmail.com>
2024-08-12 23:19:28 +0500

    animation

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// window.addEventListener("mousemove", (evt) => {
+//     let x = evt.clientX
+//     let y = evt.clientY
+//     const elBtn = document.createElement("button")
+//     elBtn.textContent = ""
+//     elBtn.style.top = `${y}px`
+//     elBtn.style.left = `${x}px`
+//     document.body.appendChild(elBtn)
+//     console.log(elBtn)
+// })
